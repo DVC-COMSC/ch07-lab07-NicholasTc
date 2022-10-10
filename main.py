874266f474669b3eac9afdd8df641e8022a0c37f
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-09 22:00:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ evenlist = []
 for i in range(len(numbers)):
     numbers[i] = int(numbers[i])
 
-for y in range(len(numbers)):
+for y in range(len(numbers) - 1, -1, -1):
     if y % 2 == 0:
-        evenlist.append(numbers[y])
+        evenlist.append(numbers.pop(y))
 
-for n in numbers:
-    if n in evenlist:
-        numbers.remove(n)
+# for n in numbers:
+#     if n in evenlist:
+#         numbers.remove(n)
 
-print(numbers)
-print(evenlist)
+evenlist.reverse()
+print("The list numbers \n", numbers)
+print("The list for even index elements\n", evenlist)
